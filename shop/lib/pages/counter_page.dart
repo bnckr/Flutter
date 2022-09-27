09f387ae86378145eccccd0271590d4bef0696a0
Bianca Delbage <bnckr@outlook.com>
2022-09-27 16:43:29 -0300

    update cart

---
+++ shop/lib/pages/counter_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:shop/Providers/counter.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  @override
+  Widget build(BuildContext context) {
+    final provider = CounterProvider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contador'),
+      ),
+      body: Column(
+        children: [
+          Text(provider?.state.value.toString() ?? '0'),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                provider?.state.inc();
+              });
+              print(provider?.state.value);
+            },
+            icon: Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                provider?.state.dec();
+              });
+              print(provider?.state.value);
+            },
+            icon: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}

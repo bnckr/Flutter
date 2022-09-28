f2f1652bc8935611cb9267788668137a42e3206a
Bianca Delbage <bnckr@outlook.com>
2022-09-28 17:11:43 -0300

    update and create

--- shop/lib/utils/app_routes.dart
+++ shop/lib/utils/app_routes.dart
@@ -1,8 +1,9 @@
 class AppRoutes {
-  static const home = '/';
+  static const home = 'home';
   static const productDetail = '/product-detail';
   static const cart = '/cart';
   static const orders = '/orders';
   static const products = '/products';
   static const productForm = '/product-form';
+  static const auth = '/';
 }

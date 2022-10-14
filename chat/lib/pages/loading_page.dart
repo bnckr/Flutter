2356c4624f02c4978f51b062aa827441723a42fb
Bianca Delbage <bnckr@outlook.com>
2022-10-14 10:19:05 -0300

    update and create

---
+++ chat/lib/pages/loading_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const CircularProgressIndicator(
+              backgroundColor: Colors.white,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Carregando...',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
